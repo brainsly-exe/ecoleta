37e7efdfd8dcb5aac1f5be5527deac7d23767def
PH-WebDeveloper <pedrobatutu@gmail.com>
2020-06-05 03:06:46 -0300

    Finalizado o backend da aplicação

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 
 import PointsController from './controllers/PointsController'
-import ItemsController from './controllers/itemsController'
+import ItemsController from './controllers/ItemsController'
 
 const routes = express.Router()
 const pointsController = new PointsController
@@ -10,6 +10,7 @@ const itemsController = new ItemsController
 routes.get('/items', itemsController.index)
 
 routes.post('/points', pointsController.create)
+routes.get('/points', pointsController.index)
 routes.get('/points/:id', pointsController.show)
 
 export default routes
